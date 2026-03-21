74e0f561738247eca05f52037b72c6a5e8a66939
Emiliano Nicolas Mendoza Garay <mendozanikolas000@gmail.com>
2026-03-20 22:24:09 -0600

    Initial config for location route

--- src/locations/dto/create-location.dto.ts
+++ src/locations/dto/create-location.dto.ts
@@ -1,1 +1,14 @@
-export class CreateLocationDto {}
+import { ArrayNotEmpty, IsArray, IsString, MaxLength } from "class-validator";
+import { Location } from '../entities/location.entity';
+
+export class CreateLocationDto{
+    @IsString()
+    @MaxLength(50)
+    locationName: string;
+    @IsString()
+    @MaxLength(100)
+    locationAddress: string;
+    @IsArray()
+    @ArrayNotEmpty()
+    locationLatLang: number[];
+}
--- src/locations/entities/location.entity.ts
+++ src/locations/entities/location.entity.ts
@@ -1,1 +1,12 @@
-export class Location {}
+import { Entity, PrimaryGeneratedColumn, Column } from 'typeorm';
+@Entity()
+export class Location {
+    @PrimaryGeneratedColumn('increment')
+    locationId: string;
+    @Column('text')
+    locationName: string;
+    @Column('text')
+    locationAddress: string;
+    @Column('array')
+    locationLatLang: number[];
+}
